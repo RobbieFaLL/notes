c27df84e5d4daf06e5da361199dcec5d973d3dfb
Robert Fall <10685211@student.bpc.ac.uk>
2024-05-02 16:49:10 +0100

    blog app

--- fallcircle/fallcircle/settings.py
+++ fallcircle/fallcircle/settings.py
@@ -16,7 +16,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "pages"
+    "pages",
+    "blog"
 ]
 #middle applied to all requests and responses
 MIDDLEWARE = [
